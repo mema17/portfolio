ee6a2984b3a6cf8a4568576615bac1c31fefddd8
mema17 <melanie.loulou@hotmail.fr>
2020-06-17 16:59:29 +0200

    Updates

---
+++ precache-manifest.b85411a2f9fe4c69e94c4a802d40b55e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96826277c90537b6ad19044cbdcc8108",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "d49c6d99951770aa7201",
+    "url": "/portfolio-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1d41e0d5f30386668a65",
+    "url": "/portfolio-react/static/js/2.80adaeb5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-react/static/js/2.80adaeb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d49c6d99951770aa7201",
+    "url": "/portfolio-react/static/js/main.fca225f9.chunk.js"
+  },
+  {
+    "revision": "f16589a8831e993e10da",
+    "url": "/portfolio-react/static/js/runtime-main.5febc6f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
